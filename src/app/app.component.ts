41c72ecce7f2ddc7c6d524d63f278289f4305222
Isuri Liyanage <36620128+liyanageisu@users.noreply.github.com>
2020-06-16 22:43:52 +0530

    4th commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 //import { slider, transform ,fader,stepper } from "./route-animations";
 import { fader } from "./route-animations";
+import { 
+  trigger,
+  transition,
+  style,
+  query,
+  group,
+  animateChild,
+  animate,
+  keyframes,
+  stagger
+} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -12,6 +23,22 @@ import { fader } from "./route-animations";
     // slider,
     // transform,
     // stepper
+
+    trigger('listAnimation',[
+      transition('* => *',[
+        query(':enter', style({opacity:0}), {optional:true}),
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75px)',offset: 0}),
+            style({opacity: 0.5, transform: 'translateY(35px)',offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',offset: 1}),
+          
+            
+          ]))
+        ]))
+
+      ])
+    ])
   ] 
 })
 export class AppComponent {
--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -5,32 +5,32 @@ import { trigger, state, style, transition, animate,query,stagger } from "@angul
   selector: 'app-background',
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.css'],
-  // animations:[
-  //   trigger('photoState',[
-  //     state('move',style({
-  //       transform: 'translateX(-100%)  translateY(50px)',
-  //     })),
-  //     state('enlarge',style({
-  //       transform: 'scale(1.5)',
-  //     })),
-  //     state('spin',style({
-  //       transform: 'rotateY(180deg) rotateZ(90deg) ',
-  //     })),
-  //     transition('spin => move', animate('200ms easw-out')),
-  //     transition('* => *', animate('500ms ease')),
-  //   ])
-  // ]
-  animations: [
-    trigger('photosAnimations',[
-      transition('* => *',[
-        query('img', style({ transform: 'translateX(-100%)'}), {optional: true}),
-        query('img', 
-          stagger('600ms', [
-            animate('900ms',style({transform: 'translateX(0)' }))
-          ]),{optional: true})
-      ])
+  animations:[
+    trigger('photoState',[
+      state('move',style({
+        transform: 'translateX(-100%)  translateY(50px)',
+      })),
+      state('enlarge',style({
+        transform: 'scale(1.5)',
+      })),
+      state('spin',style({
+        transform: 'rotateY(180deg) rotateZ(90deg) ',
+      })),
+      transition('spin => move', animate('200ms easw-out')),
+      transition('* => *', animate('500ms ease')),
     ])
   ]
+  // animations: [
+  //   trigger('photosAnimations',[
+  //     transition('* => *',[
+  //       query('img', style({ transform: 'translateX(-100%)'}), {optional: true}),
+  //       query('img', 
+  //         stagger('600ms', [
+  //           animate('900ms',style({transform: 'translateX(0)' }))
+  //         ]),{optional: true})
+  //     ])
+  //   ])
+  // ]
 })
 export class BackgroundComponent implements OnInit {
 
@@ -42,12 +42,12 @@ export class BackgroundComponent implements OnInit {
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'black';
  }
 
-  // position: string;
-  // photoUrl="../../assets/last.png"
+  position: string;
+  photoUrl="../../assets/last.png"
 
-  // changePosition(newPosition: string){
-  //     this.position = newPosition
-  // }
+  changePosition(newPosition: string){
+      this.position = newPosition
+  }
  
   
   // photo="../../assets/bg.jpg,"
